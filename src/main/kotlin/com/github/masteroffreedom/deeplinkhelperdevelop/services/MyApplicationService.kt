93c15c9f9b51947b5a2433e59d0cf65ceb33b857
GitHub Action <action@github.com>
2021-06-11 18:32:44 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/masteroffreedom/deeplinkhelperdevelop/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.masteroffreedom.deeplinkhelperdevelop.services
+
+import com.github.masteroffreedom.deeplinkhelperdevelop.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
